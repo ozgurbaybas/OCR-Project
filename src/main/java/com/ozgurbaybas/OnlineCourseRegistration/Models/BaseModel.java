1a2ea68502050ff85092a4c4bb76ef776ca52ff8
codeprex <ozgurbaybas@gmail.com>
2022-12-22 15:47:38 +0300

    Faculty and Basemodel pages added

---
+++ src/main/java/com/ozgurbaybas/OnlineCourseRegistration/Models/BaseModel.java
@@ -0,0 +1,46 @@
+package com.ozgurbaybas.OnlineCourseRegistration.Models;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private boolean isActive = true;
+
+    @Basic
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Version
+    private long version;
+
+    protected BaseModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
---
+++ src/main/java/com/ozgurbaybas/OnlineCourseRegistration/Models/Faculty.java
@@ -0,0 +1,36 @@
+package com.ozgurbaybas.OnlineCourseRegistration.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(	name = "faculties",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "name")
+        })
+public class Faculty extends BaseModel {
+
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+
+    public Faculty() {
+    }
+
+    public Faculty(String name) {
+        super();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
