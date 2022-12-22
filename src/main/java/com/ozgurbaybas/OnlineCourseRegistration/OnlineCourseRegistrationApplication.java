367ccba0efca68066dd03a9637dbc39cc6152063
codeprex <ozgurbaybas@gmail.com>
2022-12-22 14:22:57 +0300

    Project files created

---
+++ src/main/java/com/ozgurbaybas/OnlineCourseRegistration/OnlineCourseRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.ozgurbaybas.OnlineCourseRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourseRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCourseRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozgurbaybas/OnlineCourseRegistration/OnlineCourseRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozgurbaybas.OnlineCourseRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
