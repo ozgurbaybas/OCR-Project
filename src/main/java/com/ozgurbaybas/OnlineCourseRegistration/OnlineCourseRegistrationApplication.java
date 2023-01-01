5a01eb562afc2ae7477a54e32a882cc7daac7e31
codeprex <ozgurbaybas@gmail.com>
2023-01-02 01:49:46 +0300

    SpringDoc OpenAPI UI implementation completed

--- src/main/java/com/ozgurbaybas/OnlineCourseRegistration/OnlineCourseRegistrationApplication.java
+++ src/main/java/com/ozgurbaybas/OnlineCourseRegistration/OnlineCourseRegistrationApplication.java
@@ -1,7 +1,12 @@
 package com.ozgurbaybas.OnlineCourseRegistration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineCourseRegistrationApplication {
@@ -10,4 +15,13 @@ public class OnlineCourseRegistrationApplication {
 		SpringApplication.run(OnlineCourseRegistrationApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Online Course Registration")
+						.version(appVersion)
+						.description(appDesciption)
+						.license(new License().name("Online Course Registration")));
+	}
 }
